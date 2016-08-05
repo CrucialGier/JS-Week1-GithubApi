9908d82bcf0fedf9cc505eadedc052e7199ba684
Stewart Cole <stewart@>
2016-08-05 12:09:43 -0700

    Request and display info about entered user

---
+++ js/template-interface.js
@@ -0,0 +1,9 @@
+var getRepos = require('./../js/template.js').getRepos;
+
+$(document).ready(function() {
+  $("#query").click(function(event) {
+    event.preventDefault();
+    getRepos($("#requestedUser").val());
+    $('#repositoryLink').show();
+    });
+  });
--- js/template.js
+++ js/template.js
@@ -1,7 +1,24 @@
-exports.getRepos = function(){
-  $.get('https://api.github.com/users/daneden?access_token=' + apiKey).then(function(response){
+var apiKey = require('./../.env').apiKey;
+
+exports.getRepos = function(username){
+  $.get('https://api.github.com/users/' + username + '?access_token=' + apiKey).then(function(response){
     console.log(response);
-  }).fail(function(error){
-    console.log(error.responseJSON.message);
-  });
+    $("#portrait").attr('src', response.avatar_url);
+    if (response.name === null) {
+      $("#name").text("This user has not entered a name");
+    } else {
+      $("#name").text(response.name);
+    }
+    if (response.location === null) {
+      $('#location').text("User has not entered their location.");
+    } else {
+      $('#location').text(response.location);
+    }
+    if (response.email === null) {
+      $('#email').text("user has not entered an email");
+    } else {
+      $('#email').text(response.email);
+    }
+    $('#repositoryLink').attr('href', "https://github.com/" + response.login);
+  })
 };
